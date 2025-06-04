828479c7247b78ea2c4187390e94f543627247fb
FishingDonut <protronsdimi@gmail.com>
2025-06-04 13:23:56 -0300

    update game element

--- include/game_elements.h
+++ include/game_elements.h
@@ -13,7 +13,8 @@ bool gameRunning = true;
 
 int key;
 
-struct GameElements {
+struct GameElements
+{
     char person = char(36);
     char wall = char(219);
     char path = ' ';
@@ -21,10 +22,28 @@ struct GameElements {
     char newPhase = 'H';
     char door = char(35);
     char door2 = char(35);
-    char bossMap = char(33); 
-    char key = char(42); //*
-    char item = char(38); //&
-    char trap = char(63); //?
+    char bossMap = char(33);
+    char key = char(42);   //*
+    char item = char(38);  //&
+    char trap = char(63);  //?
     char enemy = char(64); //@
     char bossIcon = char(37);
+};
+
+struct MapElement
+{
+    int map[w_height][w_width];
+
+    void clear()
+    {
+        for (size_t i = 0; i < w_height; i++)
+        {
+            for (size_t j = 0; j < w_width; j++)
+            {
+                map[i][j] = 0;
+            }
+        }
+
+        return;
+    }
 };
