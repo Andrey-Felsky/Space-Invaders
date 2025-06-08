282f2fb9964d42fb95ece5d9f638fda7900e36a1
BrunoRover <brunoplay055@gmail.com>
2025-06-08 11:32:53 -0300

    refactor: refatorei o mapa e acrescentei cores aos símbolos

--- mapa/mapa.cpp
+++ mapa/mapa.cpp
@@ -1,36 +1,64 @@
 #include "mapa.h"
+#include "../utils/gameElements.cpp"
 #include <iostream>
 #include <windows.h>
 
 using namespace std;
 
+GameElements gameIcons;
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 void render() {
+    //limpa o conteudo
     for (int y = 0; y < altura; y++) {
         for (int x = 0; x < largura; x++) {
-            mapa[y][x] = ' ';
+            mapa[y][x] = gameIcons.path;
         }
     }
 
     // renderiza os inimigos
     for (int i = 0; i < 5; i++) {
         if (inimigoVivo[i]) {
-            mapa[inimigos[i][1]][inimigos[i][0]] = 'W';
+            mapa[inimigos[i][1]][inimigos[i][0]] = gameIcons.enemy;
         }
     }
 
     // renderiza os tiros
     if (tiroAtivo && tiroY >= 0)
-        mapa[tiroY][tiroX] = '|';
+        mapa[tiroY][tiroX] = gameIcons.shoot;
 
     // renderiza a nave
-    mapa[altura - 1][naveX] = '^';
+    mapa[altura - 1][naveX] = gameIcons.spaceship;
 
-    // renderiza o mapa
+    // renderiza topo do mapa
+    cout << gameIcons.wall;
+    for (int x = 0; x < largura; x++) cout << gameIcons.wall;
+    cout << gameIcons.wall << "\n";
+
+    // renderiza mapa com laterais
     for (int y = 0; y < altura; y++) {
+        cout << gameIcons.wall;
         for (int x = 0; x < largura; x++) {
-            cout << mapa[y][x];
+            char c = mapa[y][x];
+
+            //altera a cor dos simbolos
+            if (c == gameIcons.enemy) {
+                SetConsoleTextAttribute(hConsole, gameIcons.enemyColor); 
+            } else if (c == gameIcons.shoot) {
+                SetConsoleTextAttribute(hConsole, gameIcons.shootColor); 
+            } else if (c == gameIcons.spaceship) {
+                SetConsoleTextAttribute(hConsole, gameIcons.spaceshipColor); 
+            } else {
+                SetConsoleTextAttribute(hConsole, 7);  
+            }
+
+            cout << c;
         }
-        cout << endl;
+        cout << gameIcons.wall << "\n";
     }
+
+    // renderiza base do mapa
+    cout << gameIcons.wall;
+    for (int x = 0; x < largura; x++) cout << gameIcons.wall;
+    cout << gameIcons.wall << "\n";
 }
